fe0161557d3c75cfc71fc78dcc0b472467e9af83
Adria Closa <adriaclosa@gmail.com>
2022-05-06 10:39:32 +1000

    exercices solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatString = "";
 
+    for ( let i=0; i < num; i++) {
+        repeatString += string;
+    }
+    return repeatString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = Array.from(arguments[0]);
+    return newArray.filter( x => !args.includes(x))
 };
 
 // Do not edit below this line
